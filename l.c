a92a1436cf1dd3a0594a72faf6fc0ad7432d46a5
Richard Zwiren Jr <rzwirenjr@gmail.com>
2025-03-01 01:17:48 -0500

    variable width data!

--- l.c
+++ l.c
@@ -1,68 +1,62 @@
-// cl opi.c /GL /O1 /Gy /MD /DNDEBUG /link /LTCG /OPT:REF /OPT:ICF
+// cl l.c /GL /O1 /Gy /MD /DNDEBUG /link /LTCG /OPT:REF /OPT:ICF
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 typedef unsigned long long Q;typedef unsigned int D;typedef unsigned short W;typedef unsigned char B;typedef char C;
-typedef struct {D n;} AH; // header for shape 0 and 2. data is after the header.
-typedef struct {B t;D m;D n;} Z; // header for shape 3 which is only used for type 0 for read heavy operations when shape is 111; z is size of data,m is metada count;n is count of data
-typedef Q(*DV)(Q,Q); 
-typedef Q(*MV)(Q);
-// pointer is 08t6s2d48 0 in high 8 bits type in next 6 shape in next 2 and data/pointer in low 48
-// type 0 is an absolute pointer. 
-//   you may want to play with using the shape header for relative pointers when t is 0
-//   since we can address 48 bits then we can get away with relptr being 48
-Q d(Q q){return q&0x0000ffffffffffffULL;} 
-Q*p(Q q){return (Q*)d(q);} 
-B r(Q r){return d(r)+'a';}
-B v(Q v){return d(v);}
-AH* ah(Q q){return (AH*)d(q);}
-Z*  zh(Q q){return (Z*)d(q);}
+typedef struct {Q n;} AH;                                                     // header for shape 0 and 2. n is Q for alignment.
+typedef struct {B t;D m;D n;} Z;                                              // header for shape 3 which is contiguous type 0+metadata.t type; m metadata len;n data len
+typedef Q(*DV)(Q,Q);                                                          // function pointer for dyadic verb
+typedef Q(*MV)(Q);                                                            // function pointer for monadic verb
 
-Q wh(B t,D n,Q q){Q s=((~0)==n)?((!t)?3:1):(!t)?0:2;Q h=(t<<2)|s;return (h<<48)|q;}
+Q d(Q q){return q&0x0000ffffffffffffULL;}                                     // hi bits of pointer hold a header. 4 bit type 2 bit element size 2 bit shape. FUTURE: 256 byte min allocation and 256 byte alignment (8 low bits free, eliminate need for shift to get header)
+B*p(Q q){return (B*)d(q);}                                                    // util to mask out header and rebuild absolute pointer
+B r(Q r){return d(r)+'a';}                                                    // transform an atom of type 1 into ascii. This is how we do variables for now. 
+B v(Q v){return d(v);}                                                        // transform an atom of type 2 into its verb index.
 
-Q ar(Q r){return wh(1,~0,r);} // n param is unsigned so use 0xfffffffff as sentinel for atom
-Q av(Q v){return wh(2,~0,v);} 
-Q an(Q n){return wh(3,~0,n);} // needs to change when literals are bigger?
+AH* ah(Q q){return (AH*)d(q);}                                                // mask header bits and cast to vector type
+Z*  zh(Q q){return (Z*)d(q);}                                                 // mask header bits and cast to bulk vector type
 
-B h(Q q){return q>>48;} 
-B th(Q q){return h(q)>>2;} 
-B sh(Q q){return 3&h(q);} // it is potentially overkill to have room for 64 builtin types. 
+Q wh(B t,B z,B s,Q q){return (((Q)((t<<4)|((3&z)<<2)|(3&s)))<<48)|q;}         // write a header into an absolute pointer q
 
-// include a param for tyoe size
-// I want to be able to specify the size of the integer or float.
-// types are ptr0 ref1 verb2 int3 float4
-// a ref is a single column or single key value
-// a vector of ref is a dictionary
-// a vector of ref all same size vals is a table
-// this also means that a single ref that points to a vec is a single col table.
-Q tn(B t,D n){AH h={n};AH*o=malloc(sizeof(AH)+8*n);*o=h;return wh(t,n,(Q)(o+1));} // pointers are non 0 in high bits based on the type shape, no the type itself. an atom and a vector of type int will have bits set in the pointer. This means we never need to include the datatype in the preample before the data. Any pointer should be carrying the info already. 
+Q ar(Q r){return wh(1,3,1,r);}                                                // create an atom of type 1 (reference) whose element size is 8 bytes
+Q av(Q v){return wh(2,0,1,v);}                                                // create an atom of type 2 (verb)      whose element size is 1 byte
+Q an(Q n){return wh(3,3,1,n);}                                                // create an atom of type 3 (integer)   whose element size is 8 bytes
 
-// i think i am allocating an extra int in metadata, dont need pos 0 to be descrpition of self
-// this is suitable for math heavy stuff w/o depending on allocator for contig space
-// db table should be classic type 0
-//  can prolly use relptr tho
-Q zn(B t,D m,D n){                           // t is underlying type. all data must be same type for this to make sense. 
-  Z*o=malloc(sizeof(Z)+(2*4*m)+(8*n)); // allocate the size of the header plus the size of each metadata list plus the size of the data list
-  Z h={t,m,n};  // metadata is length 2m and starts at ((Z)o)+1;first m is counts next m is offsets. data starts at ((Z)o)+4*2*m). if an offset is < data pointer then we know we are dealing with a type 0.
-  *o=h;
-  return wh(0,~0,(Q)o); // set the type in pointer to 0
+B  h(Q q){return q>>48;}                                                      // header   from pointer
+B th(Q q){return h(q)>>4;}                                                    // type     from header
+B sh(Q q){return 3&h(q);}                                                     // shape    from header
+B ls(Q q){return 3&(h(q)>>2);}                                                // logeltsz from header EDGE CASE: should type 0 automatically return 3 here????
+B sz(Q q){return 1<<ls(q);}                                                   // bytesz   from logeltsz
+
+Q tn(B t,B z,D n){AH h={n};AH*o=malloc(sizeof(AH)+(1<<z)*n);*o=h;return wh(t,z,2*!!t,(Q)(o+1));} 
+Q zn(B t,B z,D m,D n){                                                        // contiguous allocation of data with same type and width, m metdata elements. n total data elements
+  Z*o=malloc(sizeof(Z)+(2*4*m)+((1<<z)*n));                                   // there are two metadata vectors of length m. all metadata is dword
+  Z h={t,m,n};*o=h;
+  return wh(0,z,3,(Q)o);
 }
 D  mz(Q a){return zh(a)->m;}
 D  nz(Q a){return zh(a)->n;}
 D* Nz(Q a){return (D*)(1+zh(a));}
 D* Oz(Q a){return mz(a)+Nz(a);}
-Q* Dz(Q a){return (Q*)((mz(a)*2)+Nz(a));}
-D  fz(Q a,D o){D m=mz(a);D z=o;while(z<m){z=Oz(a)[z];};return z;} // dist from Oz(a) to first data item 
-D  no(Q a,D o,D n){
-  D nzo=Nz(a)[o]; // number of children under this offset
-  D oo=Oz(a)[o];
+B* Dz(Q a){return (B*)((mz(a)*2)+Nz(a));}                                     // pointer to data for a bulk allocated type 0
+D  fz(Q a,D o){D m=mz(a);D z=o;while(z<m){z=Oz(a)[z];};return z;}             // dist from Oz(a) to first data item 
+D  no(Q a,D o,D n){                                                           // number of children under this offset
+  D nzo=Nz(a)[o];D oo=Oz(a)[o];
   if(oo>=mz(a)){return Nz(a)[o];}
   for(D i=0;i<nzo;i++){n+=no(a,o+i,n);}
   return n;
-} // total data items under some offset o
+}
 
-Q I(Q a,D w){B s=sh(a);return 1==sh(a)?d(a):(0==s || 2==s)?p(a)[w]:Dz(a)[w];} // not a good setup for 3==s
+// varwidth getters
+Q Bi(B* b,B z,D i){Q r=0;memcpy(&r,b+z*i,z);return r;}                        // mask this by the size of z then cast to Q. NO SIGN EXTENSION FLOATS MAY LIVE IN HERE TOO.
+Q pi(Q q,D i){return Bi(p(q),sz(q),i);}
+Q Dzi(Q q,D i){return Bi(Dz(q),sz(q),i);}
+//TODO varwidth setters
+void Bid(B* b,B z,D i,Q d){memcpy(b+z*i,&d,z);}
+void pid(Q q,D i,Q d){Bid(p(q),sz(q),i,d);}
+void Dzid(Q q,D i,Q d){Bid(Dz(q),sz(q),i,d);}
+
 D n(Q a){B s=sh(a);return 1==s?1:(0==s||2==s)?ah(a)[-1].n:3==sh(a)?(*Nz(a)):0;} 
 B t(Q a){B t;return th(a);}
 
@@ -72,132 +66,134 @@ C* VT;
 void pr(Q q){
   if(0==q){return;}
   if(0==t(q)){
-    if(3==sh(q)){for(D i=0;i<nz(q);i++){printf("%lld ",Dz(q)[i]);}}
-    if(!sh(q)){for(D i=0;i<n(q);i++){pr(p(q)[i]);}}}
+    if(3==sh(q)){for(D i=0;i<nz(q);i++){printf("%lld ",Dzi(q,i));}}
+    if(!sh(q)){for(D i=0;i<n(q);i++){pr(pi(q,i));}}}
   if(1==t(q)){printf("%c ",r(q));pr(O[d(q)]);}
   if(2==t(q)){printf("%c",VT[v(q)]);}
   if(3==t(q)){
     if(0==sh(q)){printf("?num\n");}
     if(1==sh(q)){printf("%lld\n",d(q));}
     if(2==sh(q)){
-      for(D i=0;i<n(q);i++){printf("%lld ",p(q)[i]);}}
+      for(D i=0;i<n(q);i++){printf("%lld ",pi(q,i));}}
     }
   printf("\n");
 }
 DV VD[8][4][4];
 MV VM[8][4];
 
-Q noP(Q w){Q z=tn(0,n(w));for(D i=0;i<n(w);i++){p(z)[i]=VM[1][sh(p(w)[i])](p(w)[i]);};return z;}
+Q noP(Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VM[1][sh(pi(w,i))](pi(w,i)));};return z;}
 Q noA(Q w){return an(!d(w));}
-Q noV(Q w){Q z=tn(3,n(w));for(D i=0;i<n(w);i++){p(z)[i]=!p(w)[i];};return z;}
-Q noZ(Q w){Q z=zn(3,mz(w),nz(w));for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];}for(D i=0;i<nz(w);i++){Dz(z)[i]=!Dz(w)[i];};return z;}
+Q noV(Q w){Q z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,!pi(w,i));};return z;}
+Q noZ(Q w){Q z=zn(3,ls(w),mz(w),nz(w));for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];}for(D i=0;i<nz(w);i++){Dzid(z,i,!Dzi(w,i));};return z;}
 
-Q tlA(Q w){Q z=tn(3,d(w));for(D i=0;i<d(w);i++){p(z)[i]=i;}return z;}
+Q tlA(Q w){Q z=tn(3,ls(w),d(w));for(D i=0;i<d(w);i++){pid(z,i,i);}return z;} // this will force everything to be size of whatever atom literals are.
 //Q tlV(Q v){Q z=tn(0,n(v));for(D i=0;i<n(v);i++){p(z)[i]=tlA(p(v)[i]);};return z;}
 Q tlV(Q w){
-  D nw=n(w);D mz=1+nw;D nz=0;for(D i=0;i<nw;i++){nz+=p(w)[i];};Q z=zn(3,mz,nz);
+  D nw=n(w);D mzz=1+nw;D zz=0;for(D i=0;i<nw;i++){zz+=pi(w,i);};Q z=zn(3,ls(w),mzz,zz);
   Nz(z)[0]=nw;Oz(z)[0]=1;D l=0;
-  for(D i=0;i<nw;i++){Oz(z)[i+1]=((D*)(Dz(z)+l))-Oz(z);l+=Nz(z)[i+1]=p(w)[i];}
-  D k=0;for(D i=0;i<nw;i++){for(D j=0;j<p(w)[i];j++){Dz(z)[k++]=j;}};
+  for(D i=0;i<nw;i++){Oz(z)[i+1]=((D*)(Dz(z)+l))-Oz(z);l+=Nz(z)[i+1]=pi(w,i);}
+  D k=0;for(D i=0;i<nw;i++){for(D j=0;j<pi(w,i);j++){Dzid(z,k++,j);}};
   return z;
 }
 Q tlZ(Q z){return z;}
 
 
-Q at(Q a,Q w){D sz=n(w);C ta=t(a);Q z=tn(t(a),sz);for(D i=0;i<sz;i++){p(z)[i]=p(a)[p(w)[i]];};return z;}
+Q at(Q a,Q w){Q z=tn(t(a),ls(a),n(w));for(D i=0;i<n(w);i++){pid(z,i,pi(a,pi(w,i)));};return z;}
 
 Q tp(Q a){return an(t(a));}
 Q ct(Q a){return an(n(a));}
 
-Q plVV(Q a,Q w){Q z=tn(3,n(w));for(D i=0;i<n(w);i++){p(z)[i]=p(a)[i]+p(w)[i];};return z;}
-Q plAV(Q a,Q w){Q z=tn(3,n(w));for(D i=0;i<n(w);i++){p(z)[i]=d(a)+p(w)[i];};return z;} 
+Q plVV(Q a,Q w){Q z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,pi(a,i)+pi(w,i));};return z;}
+Q plAV(Q a,Q w){Q z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,d(a)+pi(w,i));};return z;} 
 Q plVA(Q a,Q w){return plAV(w,a);}
 Q plAA(Q a,Q w){return an(d(a)+d(w));}
-Q plAP(Q a,Q w){Q z=tn(0,n(w));for(D i=0;i<n(w);i++){p(z)[i]=VD[5][1][sh(p(w)[i])](a,p(w)[i]);};return z;} 
+Q plAP(Q a,Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VD[5][1][sh(pi(w,i))](a,pi(w,i)));};return z;} 
 Q plPA(Q a,Q w){return plAP(w,a);}
-Q plPP(Q a,Q w){Q z=tn(0,n(w));for(D i=0;i<n(w);i++){p(z)[i]=VD[5][sh(p(a)[i])][sh(p(w)[i])](p(a)[i],p(w)[i]);}return z;}
-Q plPV(Q a,Q w){Q z=tn(0,n(w));for(D i=0;i<n(w);i++){p(z)[i]=VD[5][sh(p(a)[i])][1](p(a)[i],an(p(w)[i]));}return z;}
+Q plPP(Q a,Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VD[5][sh(pi(a,i))][sh(pi(w,i))](pi(a,i),pi(w,i)));}return z;}
+Q plPV(Q a,Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VD[5][sh(pi(a,i))][1](pi(a,i),an(pi(w,i))));}return z;}
 Q plVP(Q a,Q w){return plPV(w,a);}
 Q plAZ(Q a,Q w){  // this case is simple because we don't need to examine metadata, just need to copy.
-  Q z=zn(3,mz(w),nz(w));
+  Q z=zn(3,ls(w),mz(w),nz(w));
   for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];} // counts and offsets are same size
-  for(D i=0;i<nz(w);i++){Dz(z)[i]=d(a)+Dz(w)[i];}
+  for(D i=0;i<nz(w);i++){Dzid(z,i,d(a)+Dzi(w,i));}
   return z;
 } 
 Q plZA(Q a,Q w){return plAZ(w,a);}
 Q plVZ(Q a,Q w){  // this case is medium hard, metadata still the ssame but need to examine metadata to know which vector index conforms to while data index in the z list
-  Q z=zn(3,mz(w),nz(w));
+  Q z=zn(3,ls(w),mz(w),nz(w));
   for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];} // counts and offsets are same size
   Q o=*Oz(w); // o is the distance from Oz(w) to the next offset. 
   for(D i=0;i<*Nz(w);i++){
     D d=fz(w,o+i);
     for(D j=0;j<no(w,o+i,0);j++){
-      ((Q*)(Oz(z)+d))[j]=((Q*)(Oz(w)+d))[j]+p(a)[i];
+      ((Q*)(Oz(z)+d))[j]=((Q*)(Oz(w)+d))[j]+pi(a,i);
     }
   }
   return z;
 }
 Q plZV(Q a,Q w){return plVZ(w,a);}
 Q plZZ(Q a,Q w){ // not atomic in the usual sense of a k implementation. 
-  Q z=zn(3,mz(w),nz(w));
+  Q z=zn(3,ls(w),mz(w),nz(w));
   for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];}
-  for(D i=0;i<nz(w);i++){Dz(z)[i]=Dz(a)[i]+Dz(w)[i];}
+  for(D i=0;i<nz(w);i++){Dzid(z,i,Dzi(a,i)+Dzi(w,i));}
   return z;
 }
 
 Q as(Q a,Q w){O[d(a)]=w;return w;}
 
-Q caAA(Q a,Q w){Q z=tn(t(w),2);p(z)[0]=d(a);p(z)[1]=d(w);return z;}
-Q caAV(Q a,Q w){Q z=tn(t(w),1+n(w));p(z)[0]=d(a);for(D i=0;i<n(w);i++){p(z)[i+1]=p(w)[i];}return z;}
-Q caAZ(Q a,Q w){Q z=zn(t(w),1+mz(w),1+nz(w));
+Q caAA(Q a,Q w){Q z=tn(t(w),ls(w),2);pid(z,0,d(a));pid(z,1,d(w));return z;}
+Q caAV(Q a,Q w){Q z=tn(t(w),ls(w),1+n(w));pid(z,0,d(a));for(D i=0;i<n(w);i++){pid(z,i+1,pi(w,i));}return z;}
+Q caAZ(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(w),1+nz(w));
   Oz(z)[0]=1;Oz(z)[1]=((D*)Dz(z))-Oz(z);
   *Nz(z)=1+*Nz(w);
   for(D i=1;i<mz(w);i++){Oz(z)[i+1]=Oz(w)[i]+((Oz(w)[i]>=mz(w))?2:1);Nz(z)[i+1]=Nz(w)[i];}
   *Dz(z)=d(a);
-  for(D i=0;i<nz(w);i++){Dz(z)[i+1]=Dz(w)[i];}
+  for(D i=0;i<nz(w);i++){Dzid(z,i+1,Dzi(w,i));}
   return z;
 }
-Q caVA(Q a,Q w){Q z=tn(t(w),1+n(a));for(D i=0;i<n(a);i++){p(z)[i]=p(a)[i];}p(z)[n(a)]=d(a);return z;}
-Q caVV(Q a,Q w){Q z=tn(t(w),n(a)+n(w));D j=0;for(D i=0;i<n(a);i++,j++){p(z)[j]=p(a)[i];};for(D i=0;i<n(w);i++,j++){p(z)[j]=p(w)[i];}return z;}
-Q caVZ(Q a,Q w){Q z=zn(t(w),1+mz(w),n(a)+nz(w));
+Q caVA(Q a,Q w){Q z=tn(t(w),ls(w),1+n(a));for(D i=0;i<n(a);i++){pid(z,i,pi(a,i));}pid(z,n(a),d(a));return z;}
+Q caVV(Q a,Q w){Q z=tn(t(w),ls(w),n(a)+n(w));D j=0;for(D i=0;i<n(a);i++,j++){pid(z,j,pi(a,i));};for(D i=0;i<n(w);i++,j++){pid(z,j,pi(w,i));}return z;}
+Q caVZ(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(w),n(a)+nz(w));
   Oz(z)[0]=1;Oz(z)[1]=((D*)Dz(z))-Oz(z);
   *Nz(z)=1+*Nz(w);Nz(z)[1]=n(a);
-  for(D i=1;i<mz(w);i++){Oz(z)[i+1]=Oz(w)[i]+((Oz(w)[i]>=mz(w))?1+n(a)*2:1);Nz(z)[i+1]=Nz(w)[i];}
-  for(D i=0;i<n(a);i++){Dz(z)[i]=p(a)[i];}
-  for(D i=0;i<nz(w);i++){Dz(z)[i+n(a)]=Dz(w)[i];}
+  for(D i=1;i<mz(w);i++){Oz(z)[i+1]=Oz(w)[i]+((Oz(w)[i]>=mz(w))?1+n(a)*2:1);Nz(z)[i+1]=Nz(w)[i];} //ls(w) missing
+  for(D i=0;i<n(a);i++){Dzid(z,i,pi(a,i));}
+  for(D i=0;i<nz(w);i++){Dzid(z,i+n(a),Dzi(w,i));}
   return z;
 }
-Q caZA(Q a,Q w){Q z=zn(t(w),1+mz(a),1+nz(a));
+Q caZA(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(a),1+nz(a));
   Oz(z)[0]=1;*Nz(z)=1+*Nz(a);
   for(D i=1;i<2*mz(a);i++){Nz(z)[i]=Nz(a)[i];}
-  for(D i=0;i<nz(a);i++){Dz(z)[i]=Dz(a)[i];}
-  Nz(z)[mz(a)]=1;Oz(z)[mz(a)]=((D*)(Dz(z)+nz(a)))-Oz(z);Dz(z)[nz(a)]=d(w);
+  for(D i=0;i<nz(a);i++){Dzid(z,i,Dzi(a,i));}
+  Nz(z)[mz(a)]=1;Oz(z)[mz(a)]=((D*)(Dz(z)+nz(a)))-Oz(z);Dzid(z,nz(a),d(w));
   return z;
 }
-Q caZV(Q a,Q w){Q z=zn(t(w),1+mz(a),n(w)+nz(a));
+Q caZV(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(a),n(w)+nz(a));
   Oz(z)[0]=1;*Nz(z)=1+*Nz(a);
   for(D i=1;i<2*mz(a);i++){Nz(z)[i]=Nz(a)[i];}
-  for(D i=0;i<nz(a);i++){Dz(z)[i]=Dz(a)[i];}
+  for(D i=0;i<nz(a);i++){Dzid(z,i,Dzi(a,i));}
   Nz(z)[mz(a)]=1;Oz(z)[mz(a)]=((D*)(Dz(z)+nz(a)))-Oz(z);
-  for(D i=0;i<n(w);i++){Dz(z)[nz(a)+i]=p(w)[i];}
+  for(D i=0;i<n(w);i++){Dzid(z,nz(a)+i,pi(w,i));}
 
   return z;
 }
-Q caZZ(Q a,Q w){Q z=zn(t(w),mz(a)+mz(w),nz(a)+nz(w));
+Q caZZ(Q a,Q w){Q z=zn(t(w),ls(w),mz(a)+mz(w),nz(a)+nz(w));
   D i=1,j=0;
   Oz(z)[0]=1;*Nz(z)=(*Nz(a))+(*Nz(w));
   printf("nz(z) %d *Nz(z) %d\n",nz(z),*Nz(z));
-  for(j=1;j<*Nz(a);j++,i++){Oz(z)[i]=Oz(a)[j];Nz(z)[i]=Oz(a)[j];}
+  for(j=1;j<*Nz(a);j++,i++){Oz(z)[i]=Oz(a)[j];Nz(z)[i]=Oz(a)[j];} // wrong
   for(j=1;j<*Nz(w);j++,i++){Oz(z)[i]=Oz(z)[*Nz(a)]+Oz(w)[j]; Nz(z)[i]=Oz(w)[j];}
   for(j=*Nz(a);j<mz(a);j++,i++){Oz(z)[i]=Oz(z)[*Nz(a)+*Nz(w)-1]+Oz(a)[j];Nz(z)[i]=Oz(a)[j];}
   for(j=*Nz(w);j<mz(w);j++,i++){Oz(z)[i]=Oz(z)[ mz(a)+*Nz(w)-1]+Oz(w)[j];Nz(z)[i]=Oz(w)[j];}
-  for(i=0,j=0;i<nz(a);i++,j++){Dz(z)[i]=Dz(a)[j];}
-  for(j=0;j<nz(w);i++,j++){Dz(z)[i]=Dz(w)[j];}
+  for(i=0,j=0;i<nz(a);i++,j++){Dzid(z,i,Dzi(a,j));}
+  for(j=0;j<nz(w);i++,j++){Dzid(z,i,Dzi(w,j));}
   return z;
 }
 
-
 Q id(Q a){return a;}
+Q en(Q a){Q z=tn(0,3,1);pid(z,0,a);return z;}
+Q enA(Q a){Q z=tn(t(a),ls(a),1);pid(z,0,d(a));return z;}
+
 DV VD[8][4][4]={
   {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}},
   {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}},
@@ -215,7 +211,7 @@ MV VM[8][4]={
   {ct,ct,ct,ct},
   {0,0,0,0}, // consider making this x<<1
   {id,id,id,id},
-  {0,0,0,0}};
+  {en,enA,en,en}};
 C* VT=" ~!@#+:,";
 
 Q e(Q* q);
@@ -256,32 +252,13 @@ D main(void){
 
 /**
  * Can I come up with a better type 0? 
- *  issue is that we have to store absolute pointers which can point anywhere in memory.
- *  I want to make sure that certain allocations happen in contiguous spaces at once. 
  *  
- *  suppose l:(0;1 2;(3 4;(5 6;7 8;9 10 11 12 13 14 15));16;17 18)
- * 
- *  I want
- *  (5 1 2 2 2 3 2 2 7 1  2 ; n
- *   0 0 1 3 3 5 5 7 9 16 17; offset from base
- *   0 0 0 0 3 3 5 5 5 0  0 ; parent
- *   0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18) data
- *  
- * even better would be
- *  (5 1 2 2 1  2  2 3 2 2 7 ; n
- *   0 0 1 3 16 17 3 5 5 7 9 ; offset from base
- *   0 0 0 0 0  0  3 3 5 5 5 ; parent
- *   0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18) data
- * so that all elements would be stored in depth order. 
- * the size of the metadata vectors is related to the size of each of the lists.
- * note that rows where parent=offset from base are the start of sublists
- * 
- * even even better would be to eliminate the parent and instead use first child
- * 
+ * current setup for shape 3 is
  * ( 5 1 2 2 1  2  2 3 2 2 7 ; n
- *   0 0 1 3 16 17 3 5 5 7 9 ; offset from base
- *   1 0 0 6 0  0  0 9 0 0 0 ; first child (0 if no children)
+ *   1 0 1 3 16 17 3 5 5 7 9 ; offset from base to first child
  *   0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18) data
+ * 
+ *  algorithms involving shape 3 are tedious to write... can you use simpler apis for basic pointers to implement the metadata manipulations for shape 3?                                                   
  *  
  *  versus
  *  p0->[a1,p2,p3,a2,p9]
@@ -300,20 +277,10 @@ D main(void){
  *   h                           256 00000000
  *   i                           512 000000000
  * 
- * and so on.
- * 
- * atoms are annoying. they reduce regularity in the codebase in order to avoid fetches from memory
- *  you need to believe that atoms are common in order to build this into the system. 
- *  
- * how can I take advantage of not having atoms in my language? 
- *  the point of atoms is that they are easy to pass around. They look the same as any other data
- *  but you don't need to fetch the actual data from memory
- *  you need to find a super nice encoding that allows for    
- * 
- * is there really a need to do type checking in the evaluator? 
- *  the types are float and int
- *  the sizes of each of the types can vary
- *  the presentation of each of the types can vary
- * 
+ *   and so on.
+ *   Address space is plentiful. even with 512 byte alignment we have room for 2^39~=549 billion allocations.
+ *   That's far more than any practical workload would need. 
+ *    That makes me want to do saves to disk with nearly raw memory dumps. Run length encoding would compress awesomely. 
+ *    a smaller alignment would allow for mapping in place without blowing out disk too much though... 
  * 
  */

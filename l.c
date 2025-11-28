bfd21bb182e7ef8a140280c9fa844c7f8193fa43
nomorerepl <rzwirenjr@gmail.com>
2025-11-28 10:11:13 -0500

    modadic/dyadic broadcast and verb dispatch changes

--- l.c
+++ l.c
@@ -14,27 +14,30 @@ AH* ah(Q q){return (AH*)q;}                                                   //
 Z*  zh(Q q){return (Z*)q;}                                                    // if it is known that we are dealing with shape 3 Z struct then we can get that in its entirety.
 
 B ip(Q q){return !(7&q);}                                                     // Is this Q a pointer? nonzero in low 3 bits means atom
-Q d(Q q){return q>>3;}                                                        // shift out the flags. decodes small integers, needs to be en
+Q d(Q q){return q>>3;}                                                        // shift out the flags. decodes small integers
 B*p(Q q){return (B*)(ah(q)+1);}                                               // pointers need no manipulation, low bits==0 is the signal for a pointer.
 B r(Q r){return d(r)+'a';}                                                    // transform an atom of type 1 into ascii. This is how we do variables for now. 
 B v(Q v){return d(v);}                                                        // transform an atom of type 2 into its verb index.
+B c(Q c){return d(c);}                                                        // transform an atom of type 5 into its control sentinel
 
 Q ar(Q r){return (r<<3)|1;}                                                   // create an atom of type 1 (reference)
 Q av(Q v){return (v<<3)|2;}                                                   // create an atom of type 2 (verb)      
-Q an(Q n){return (n<(1ULL<<61))?((n<<3)|3):3;}                                // create an atom of type 3 (integer) TODO: heap allocated 64 bit int. return 3 as a tagged 0 for stuff that should really be allocated on heap. 
+Q an(Q n){return (n<(1ULL<<61))?((n<<3)|3):(0|3);}                            // create an atom of type 3 (integer) TODO: heap allocated 64 bit int. return 3 as a tagged 0 for stuff that should really be allocated on heap. 
 Q ap(Q v){return (v<<3)|4;}                                                   // create an atom of type 4 (partial eval)
+Q ac(Q c){return (c<<3)|5;}                                                   // create an atom of type 5 (control sentinel)
+Q et(Q q,B t){return 0==t?q:1==t?ar(q):2==t?av(q):3==t?an(q):4==t?ap(q):0;}   // encode data of an atom based on the type
 
 B ia(Q q){return !ip(q);}                                                     // is atom  from pointer
-B th(Q q){return ia(q)?(7&q):ah(q)->t;}                                         // type     from header
-B sh(Q q){return ia(q)?1:ah(q)->s;}                                           // shape    from header
+B th(Q q){return ia(q)?(7&q):ah(q)->t;}                                       // type     from header
+B sh(Q q){return ia(q)?0:ah(q)->s;}                                           // shape    from header
 B ls(Q q){return ia(q)?3:ah(q)->z;}                                           // logeltsz from header EDGE CASE: should type 0 automatically return 3 here????
 B sz(Q q){return 1<<ls(q);}                                                   // bytesz   from logeltsz
 
 Q tsn(B t,B s,B z,D n){AH h={t,s,z,0,n};AH*o=malloc(sizeof(AH)+(1<<z)*n);*o=h;return (Q)o;}  // LATER: custom allocator. Q points at data not header 
-Q tn(B t,B z,D n){return tsn(t,2*!!t,z,n);}
+Q tn(B t,B z,D n){return tsn(t,1,z,n);}
 Q zn(B t,B z,D m,D n){                                                        // contiguous allocation of data with same type and width, m metdata elements. n total data elements
   Z*o=malloc(sizeof(Z)+(2*sizeof(D)*m)+((1<<z)*n));                           // there are two metadata vectors of length m. all metadata is dword
-  AH hh={t,3,z,0,n};
+  AH hh={t,2,z,0,n};
   Z h={hh,t,m,n};*o=h;
   return (Q)o;
 }
@@ -56,14 +59,14 @@ D  no(Q a,D o,D n){                                                           //
 Q Bi(B* b,B z,D i){Q r=0;memcpy(&r,b+z*i,z);return r;}                        // mask this by the size of z then cast to Q. NO SIGN EXTENSION FLOATS MAY LIVE IN HERE TOO.
 Q pi(Q q,D i){return Bi(p(q),sz(q),i);}
 Q Dzi(Q q,D i){return Bi(Dz(q),sz(q),i);}
-Q Gi(Q q,B z,B s,D i){return 1==s?d(q):(2==s||0==s)?Bi(p(q),z,i):Bi(Dz(q),z,i);}   // universal getter, checks shape.
-//TODO varwidth setters
+Q Gi(Q q,B z,B s,D i){return 0==s?d(q):(1==s)?Bi(p(q),z,i):2==s?Bi(Dz(q),z,i):0;}   // universal getter, checks shape. Eventually this will need to be able to support heap allocated atoms. right now it assumes all atoms are stuffed into the pointers.
+Q Giv(Q q,D i){B s=sh(q),tq=th(q);return 0==s?q:1==s?et(pi(q,i),tq):2==s?et(Dzi(q,i),tq):0;}  // universal getter, returns tagged instead of raw
+// varwidth setters
 void Bid(B* b,B z,D i,Q d){memcpy(b+z*i,&d,z);}
 void pid(Q q,D i,Q d){Bid(p(q),sz(q),i,d);}
 void Dzid(Q q,D i,Q d){Bid(Dz(q),sz(q),i,d);}
-// void Si(Q q,B z,B s,D i,Q d){1==s?((h(q)<<48)|d):(2==s||0==s)?Bid(p(q),z,i,d):Bid(Dz(q),z,i,d);}   // universal setter
 
-D n(Q a){B s=sh(a);return 1==s?1:(0==s||2==s)?ah(a)->n:3==sh(a)?nz(a):0;} 
+D n(Q a){B s=sh(a);return 0==s?1:1==s?ah(a)->n:2==s?nz(a):0;} 
 B t(Q a){return th(a);}
 
 Q O[26];
@@ -72,179 +75,179 @@ C* VT;
 void pr(Q q){
   if(0==q){return;}
   if(0==t(q)){
-    if(3==sh(q)){for(D i=0;i<n(q);i++){printf("%lld ",Dzi(q,i));}}
-    if(!sh(q)){for(D i=0;i<n(q);i++){pr(pi(q,i));}}}
+    if(!sh(q)){printf("atom type 0?%lld ",q);}
+    if(2==sh(q)){for(D i=0;i<n(q);i++){pr(Dzi(q,i));}}
+    if(1==sh(q)){for(D i=0;i<n(q);i++){pr(pi(q,i));}}}
   if(1==t(q)){printf("%c ",r(q));pr(O[d(q)]);}
   if(2==t(q)){printf("%c",VT[v(q)]);}
   if(3==t(q)){
-    if(0==sh(q)){printf("?num");}
-    if(1==sh(q)){printf("%lld",d(q));}
-    if(2==sh(q)){
-      for(D i=0;i<n(q);i++){printf("%lld ",pi(q,i));}}
-    }
-    if(3==sh(q)){for(D i=0;i<n(q);i++){printf("%lld ",Dzi(q,i));}}
+    if(0==sh(q)){printf("%lld",d(q));}
+    if(1==sh(q)){for(D i=0;i<n(q);i++){printf("%lld ",pi(q,i));}}
+    if(2==sh(q)){for(D i=0;i<n(q);i++){printf("%lld ",Dzi(q,i));}}
+  }
   if(4==t(q)){for(D i=0;i<n(q);i++){pr(pi(q,i));}}
+  if(5==t(q)){printf("control: %d\n",c(q));}
   printf("\n");
 }
-DV VD[8][4][4];
-MV VM[8][4];
-
-Q noP(Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VM[1][sh(pi(w,i))](pi(w,i)));};return z;}
-Q noA(Q w){return an(!d(w));}
-Q noV(Q w){Q z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,!pi(w,i));};return z;}
-Q noZ(Q w){Q z=zn(3,ls(w),mz(w),n(w));for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];}for(D i=0;i<n(w);i++){Dzid(z,i,!Dzi(w,i));};return z;}
-
-Q tlA(Q w){Q z=tn(3,ls(w),d(w));for(D i=0;i<d(w);i++){pid(z,i,i);}return z;} // this will force everything to be size of whatever atom literals are.
-//Q tlV(Q v){Q z=tn(0,n(v));for(D i=0;i<n(v);i++){p(z)[i]=tlA(p(v)[i]);};return z;}
-Q tlV(Q w){
-  D nw=n(w);D mzz=1+nw;D zz=0;for(D i=0;i<nw;i++){zz+=pi(w,i);};Q z=zn(3,ls(w),mzz,zz);
-  Nz(z)[0]=nw;Oz(z)[0]=1;D l=0;
-  for(D i=0;i<nw;i++){Oz(z)[i+1]=((D*)(Dz(z)+l))-Oz(z);l+=Nz(z)[i+1]=pi(w,i);}
-  D k=0;for(D i=0;i<nw;i++){for(D j=0;j<pi(w,i);j++){Dzid(z,k++,j);}};
-  return z;
-}
-Q tlZ(Q z){return z;}
-
-
-Q at(Q a,Q w){Q z=tn(t(a),ls(a),n(w));for(D i=0;i<n(w);i++){pid(z,i,pi(a,pi(w,i)));};return z;}
+DV VD[9];
+MV VM[9];
 
 Q tp(Q a){return an(t(a));}
-Q ct(Q a){return an(3==sh(a)?*Nz(a):n(a));}
+Q ct(Q a){return an(2==sh(a)?*Nz(a):n(a));}
 
-Q plVV(Q a,Q w){Q z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,pi(a,i)+pi(w,i));};return z;}
-Q plAV(Q a,Q w){Q z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,d(a)+pi(w,i));};return z;} 
-Q plVA(Q a,Q w){return plAV(w,a);}
-Q plAA(Q a,Q w){return an(d(a)+d(w));}
-Q plAP(Q a,Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VD[5][1][sh(pi(w,i))](a,pi(w,i)));};return z;} 
-Q plPA(Q a,Q w){return plAP(w,a);}
-Q plPP(Q a,Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VD[5][sh(pi(a,i))][sh(pi(w,i))](pi(a,i),pi(w,i)));}return z;}
-Q plPV(Q a,Q w){Q z=tn(0,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,VD[5][sh(pi(a,i))][1](pi(a,i),an(pi(w,i))));}return z;}
-Q plVP(Q a,Q w){return plPV(w,a);}
-Q plAZ(Q a,Q w){  // this case is simple because we don't need to examine metadata, just need to copy.
-  Q z=zn(3,ls(w),mz(w),n(w));
-  for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];} // counts and offsets are same size
-  for(D i=0;i<n(w);i++){Dzid(z,i,d(a)+Dzi(w,i));}
-  return z;
-} 
-Q plZA(Q a,Q w){return plAZ(w,a);}
-Q plVZ(Q a,Q w){  // this case is medium hard, metadata still the ssame but need to examine metadata to know which vector index conforms to while data index in the z list
-  Q z=zn(3,ls(w),mz(w),n(w));
-  for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];} // counts and offsets are same size
-  Q o=*Oz(w); // o is the distance from Oz(w) to the next offset. 
-  for(D i=0;i<*Nz(w);i++){
-    D d=fz(w,o+i);
-    for(D j=0;j<no(w,o+i,0);j++){
-      ((Q*)(Oz(z)+d))[j]=((Q*)(Oz(w)+d))[j]+pi(a,i);
+Q dvb(Q a,Q w,DV dv){ B ta=t(a),tw=t(w),sa=sh(a),sw=sh(w);D cta=d(ct(a)),ctw=d(ct(w));// dyadic verb broadcast
+  Q z;
+  if(!ta&&!tw){
+    if(cta!=ctw){return ac(2);} z=tn(0,3,cta);
+    for(D i=0;i<cta;i++){
+      Q ai=Giv(a,i);Q wi=Giv(w,i);Q zi=dv(ai,wi);
+      if(5==t(zi)){return zi;} // sentinel bubbled up. later: add cleanup
+      pid(z,i,zi);
     }
+    return z;
+  }
+  if(!ta){
+    if(1==sa){z=tn(0,3,cta);
+      if(2==sw){return ac(3);}
+      // otherwise shape 0 or 1, can use GI for both. 
+      if(1==sw && cta!=ctw){return ac(2);} // length
+      for(D i=0;i<cta;i++){Q ai=Giv(a,i);Q wi=Giv(w,i);Q zi=dv(ai,wi);
+        if(5==t(zi)){return zi;} // sentinel bubbled up. later: add cleanup
+        pid(z,i,zi);
+      }
+      return z;
+    }
+    if(2==sa){return ac(3);}
+  }
+  if(!tw){
+    if(1==sw){z=tn(0,3,ctw);
+      if(2==sa){return ac(3);}
+      // otherwise shape 0 or 1, can use GI for both. 
+      if(1==sa && cta!=ctw){return ac(2);} // length
+      for(D i=0;i<ctw;i++){Q ai=Giv(a,i);Q wi=Giv(w,i);Q zi=dv(ai,wi);
+        if(5==t(zi)){return zi;} // sentinel bubbled up. later: add cleanup
+        pid(z,i,zi);
+      }
+      return z;
+    }
+    if(2==sw){return ac(3);}
+  }
+  if(cta!=ctw && !(1==cta || 1==ctw)){return ac(2);}
+  return ac(0); // if we get here then a and w are not type 0 and length is conforming so work can be done on them
+}
+Q mvb(Q w,MV mv){ // monadic verb broadcast. will return data if it broadcasts. if not it returns a sentinel to communicate upwards either a length error or for the verb to do the work. 
+  Q z;B tw=t(w),sw=sh(w);D nw=n(w);
+  if(0==tw){
+    z=1==sw?tn(0,3,nw):zn(0,3,mz(w),nw);
+    for(D i=0;i<nw;i++){Q wi=Giv(w,i);Q zi=mv(wi);
+      if(5==t(zi)){return zi;}
+      if(1==sw){pid(z,i,zi);} else {Dzid(z,i,zi);}
+    }
+    return z;
+  }
+  return ac(0); // this is a signal to the verb that work should be done on the w parameter.
+}
+Q car(Q w){
+  B s=sh(w);
+  if(0==s){return w;}
+  if(2==s){
+    D n=*Nz(w);  //get the length of the first thing in the z list
+    Q z=tn(t(w),ls(w),n);
+    for(D i=0;i<n;i++){pid(z,i,Dzi(w,i));} // later: memcpy the range. 
+    return z;
+  }
+  Q d=pi(w,0);
+  if(0==t(w)){return d;}
+  return et(d,t(w));
+}
+
+Q nt(Q w){
+  Q z=mvb(w,nt);
+  if(5!=t(z)){return z;}if(c(z)){return z;} // if data is returned, return the data. if a control sentinel is returned and its payload is nonzero then return, otherwise listen to theh control signal to do the proper work.
+  if(ia(w)){return an(!d(w));}
+  if(1==sh(w)){z=tn(3,ls(w),n(w));for(D i=0;i<n(w);i++){pid(z,i,!pi(w,i));};return z;}
+  if(2==sh(w)){z=zn(3,ls(w),mz(w),n(w));for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];}for(D i=0;i<n(w);i++){Dzid(z,i,!Dzi(w,i));};return z;}
+  return ac(1); // return shape error
+}
+
+Q tl(Q w){
+  if(ia(w)){Q z=tn(3,ls(w),d(w));for(D i=0;i<d(w);i++){pid(z,i,i);}return z;}
+  if(1==sh(w)){
+    D nw=n(w);D mzz=1+nw;D zz=0;for(D i=0;i<nw;i++){zz+=pi(w,i);};Q z=zn(3,ls(w),mzz,zz);
+    Nz(z)[0]=nw;Oz(z)[0]=1;D l=0;
+    for(D i=0;i<nw;i++){Oz(z)[i+1]=((D*)(Dz(z)+l))-Oz(z);l+=Nz(z)[i+1]=pi(w,i);}
+    D k=0;for(D i=0;i<nw;i++){for(D j=0;j<pi(w,i);j++){Dzid(z,k++,j);}};
+    return z;
+  }
+  if(2==sh(w)){return ac(3);} // nyi
+  return ac(1); // shape err
+}
+
+Q at(Q a,Q w){
+  B aa=ia(w);B tz=t(a);B nz=n(w);B shz=sh(w);Q z=2==shz?zn(tz,sz(a),mz(w),nz):tn(t(a),ls(a),nz);
+  for(D i=0;i<nz;i++){
+    Q wi=Gi(w,sz(w),shz,i);Q zi;
+    if(2==sh(a)){
+      D nz=Nz(a)[d(w)];D oz=Oz(a)[wi];
+      zi=tn(t(a),ls(a),nz);for(D i=0;i<nz;i++){pid(z,i,Bi(Dz(a)+oz,sz(a),i));} // later: memcpy the whole range instead.
+      printf("sh(zi) %d t(zi) %d\n",sh(zi),t(zi));pr(zi);
+    } else {
+      zi=Gi(a,sz(a),sh(a),wi);
+    }
+    if(2==shz){Dzid(z,i,zi);} else {pid(z,i,zi);}
+  }
+  return aa?car(z):z;
+}
+
+Q math(Q a,Q w,DV op){ // anything that gets here has been broadcasted. we can use the universal getter on both a and w
+  D na=n(a),nw=n(w);D nz=na<nw?nw:na;B shz=sh(a)<sh(w)?sh(w):sh(a);B lz=ls(a)<ls(w)?ls(w):ls(a);
+  if(0==shz){return an(op(d(a),d(w)));}
+  Q z=1==shz?tn(t(a),lz,nz):zn(t(a),lz,mz(a),nz); // later: set the metadata for shape 2, it is unset.
+  for(D i=0;i<nz;i++){Q ai=Gi(a,sz(a),sh(a),i);Q wi=Gi(w,sz(w),sh(w),i);
+    Q zi=op(ai,wi);
+    if(1==shz){pid(z,i,zi);} else {Dzid(z,i,zi);}
   }
   return z;
 }
-Q plZV(Q a,Q w){return plVZ(w,a);}
-Q plZZ(Q a,Q w){ // not atomic in the usual sense of a k implementation. 
-  Q z=zn(3,ls(w),mz(w),n(w));
-  for(D i=0;i<2*mz(w);i++){Nz(z)[i]=Nz(w)[i];}
-  for(D i=0;i<n(w);i++){Dzid(z,i,Dzi(a,i)+Dzi(w,i));}
-  return z;
-}
+Q pl_aa(Q a,Q w){ return a+w;}
+Q ml_aa(Q a,Q w){ return a*w;}
+Q pl(Q a,Q w){Q z=dvb(a,w,pl);if(5!=t(z)){return z;}if(c(z)){return z;}; return math(a,w,pl_aa);} // later: float support and type promotion. 
+Q ml(Q a,Q w){Q z=dvb(a,w,ml);if(5!=t(z)){return z;}if(c(z)){return z;}; return math(a,w,ml_aa);}
 
 Q as(Q a,Q w){O[d(a)]=w;return w;}
 
-// LATER: finish caPA caPV caPZ
-Q caPA(Q a,Q w){Q z=tn(t(a)==t(w)?t(a):0,3,1+n(a));for(D i=0;i<n(a);i++){pid(z,i,pi(a,i));}pid(z,n(a),d(a));return z;}
-Q caPV(Q a,Q w){Q z=tn(t(a)==t(w)?t(a):0,3,n(a)+n(w));D j=0;for(D i=0;i<n(a);i++,j++){pid(z,j,pi(a,i));};for(D i=0;i<n(w);i++,j++){pid(z,j,pi(w,i));}return z;}
-Q caPZ(Q a,Q w){Q z=tn(t(a)==t(w)?t(a):0,3,n(a)+1);
-  for(D i=0;i<n(a);i++){pid(z,i,pi(a,i));};pid(z,n(a),w);
-  return z;
-}
-Q caAA(Q a,Q w){Q z=tn(t(w),ls(w),2);pid(z,0,d(a));pid(z,1,d(w));return z;}
-Q caAV(Q a,Q w){Q z=tn(t(w),ls(w),1+n(w));pid(z,0,d(a));for(D i=0;i<n(w);i++){pid(z,i+1,pi(w,i));}return z;}
-Q caAZ(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(w),1+n(w));
-  Oz(z)[0]=1;Oz(z)[1]=((D*)Dz(z))-Oz(z);
-  *Nz(z)=1+*Nz(w);
-  for(D i=1;i<mz(w);i++){Oz(z)[i+1]=Oz(w)[i]+((Oz(w)[i]>=mz(w))?2:1);Nz(z)[i+1]=Nz(w)[i];}
-  *Dz(z)=d(a);
-  for(D i=0;i<n(w);i++){Dzid(z,i+1,Dzi(w,i));}
-  return z;
-}
-Q caVA(Q a,Q w){Q z=tn(t(w),ls(w),1+n(a));for(D i=0;i<n(a);i++){pid(z,i,pi(a,i));}pid(z,n(a),d(a));return z;}
-Q caVV(Q a,Q w){Q z=tn(t(w),ls(w),n(a)+n(w));D j=0;for(D i=0;i<n(a);i++,j++){pid(z,j,pi(a,i));};for(D i=0;i<n(w);i++,j++){pid(z,j,pi(w,i));}return z;}
-Q caVZ(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(w),n(a)+n(w));
-  Oz(z)[0]=1;Oz(z)[1]=((D*)Dz(z))-Oz(z);
-  *Nz(z)=1+*Nz(w);Nz(z)[1]=n(a);
-  for(D i=1;i<mz(w);i++){Oz(z)[i+1]=Oz(w)[i]+((Oz(w)[i]>=mz(w))?1+n(a)*2:1);Nz(z)[i+1]=Nz(w)[i];} //ls(w) missing
-  for(D i=0;i<n(a);i++){Dzid(z,i,pi(a,i));}
-  for(D i=0;i<n(w);i++){Dzid(z,i+n(a),Dzi(w,i));}
-  return z;
-}
-Q caZA(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(a),1+n(a));
-  Oz(z)[0]=1;*Nz(z)=1+*Nz(a);
-  for(D i=1;i<2*mz(a);i++){Nz(z)[i]=Nz(a)[i];}
-  for(D i=0;i<n(a);i++){Dzid(z,i,Dzi(a,i));}
-  Nz(z)[mz(a)]=1;Oz(z)[mz(a)]=((D*)(Dz(z)+n(a)))-Oz(z);Dzid(z,n(a),d(w));
-  return z;
-}
-Q caZV(Q a,Q w){Q z=zn(t(w),ls(w),1+mz(a),n(w)+n(a));
-  Oz(z)[0]=1;*Nz(z)=1+*Nz(a);
-  for(D i=1;i<2*mz(a);i++){Nz(z)[i]=Nz(a)[i];}
-  for(D i=0;i<n(a);i++){Dzid(z,i,Dzi(a,i));}
-  Nz(z)[mz(a)]=1;Oz(z)[mz(a)]=((D*)(Dz(z)+n(a)))-Oz(z);
-  for(D i=0;i<n(w);i++){Dzid(z,n(a)+i,pi(w,i));}
-
-  return z;
-}
-Q caZZ(Q a,Q w){Q z=zn(t(w),ls(w),mz(a)+mz(w),n(a)+n(w));
-  D i=1,j=0;
-  Oz(z)[0]=1;*Nz(z)=(*Nz(a))+(*Nz(w));
-  for(j=1;j<*Nz(a);j++,i++){Oz(z)[i]=Oz(a)[j];Nz(z)[i]=Nz(a)[j];}
-  for(j=1;j<*Nz(w);j++,i++){Oz(z)[i]=Oz(z)[*Nz(a)]+Oz(w)[j]; Nz(z)[i]=Nz(w)[j];}
-  for(j=*Nz(a);j<mz(a);j++,i++){Oz(z)[i]=Oz(z)[*Nz(a)+*Nz(w)-1]+Oz(a)[j];Nz(z)[i]=Nz(a)[j];}
-  for(j=*Nz(w);j<mz(w);j++,i++){Oz(z)[i]=Oz(z)[ mz(a)+*Nz(w)-1]+Oz(w)[j];Nz(z)[i]=Nz(w)[j];}
-  for(i=0,j=0;i<n(a);i++,j++){Dzid(z,i,Dzi(a,j));}
-  for(j=0;j<n(w);i++,j++){Dzid(z,i,Dzi(w,j));}
+Q ca(Q a,Q w){
+  B tz=((t(a)==t(w))&&2!=sh(a)&&2!=sh(w))?t(a):0;
+  D j=0;D ca=d(ct(a));D cw=d(ct(w));Q z=tn(tz,3,ca+cw);
+  for(D i=0;i<ca;i++,j++){Q ai=at(a,an(i));pid(z,j,tz?d(ai):ai);} // decode if not type 0
+  for(D i=0;i<cw;i++,j++){Q wi=at(w,an(i));pid(z,j,tz?d(wi):wi);} // decode if atom or somethig
   return z;
 }
 
 Q id(Q a){return a;}
-Q en(Q a){Q z=tn(0,3,1);pid(z,0,a);return z;}
-Q enA(Q a){Q z=tn(t(a),ls(a),1);pid(z,0,d(a));return z;}
+Q en(Q a){Q z=tn(ia(a)?t(a):0,ls(a),1);pid(z,0,ia(a)?d(a):a);return z;}
 
-DV VD[8][4][4]={
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}},
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}},
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}},
-  {{0,0,0,0},{0,0,0,0},{0,at,0,0},{0,0,0,0}}, //at
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}},
-  {{plPP,plPA,plPV,0},{plAP,plAA,plAV,plAZ},{plVP,plVA,plVV,plVZ},{0,plZA,plZV,plZZ}}, //pl,
-  {{0,0,0,0},{as,as,as,as},{0,0,0,0},{0,0,0,0}}, // as
-  {{caVV,caVA,caVV,caVZ},{0,caAA,caAV,caAZ},{caVV,caVA,caVV,caVZ},{0,caZA,caZV,caZZ}}}; // ca
-MV VM[8][4]={
-  {0,0,0,0},
-  {noP,noA,noV,noZ},
-  {0,tlA,tlV,0},
-  {tp,tp,tp,tp},
-  {ct,ct,ct,ct},
-  {0,0,0,0}, // consider making this x<<1
-  {id,id,id,id},
-  {en,enA,en,en}};
-C* VT=" ~!@#+:,";
+DV VD[9]={0,0,0,at,0,pl,ml,as,ca};
+MV VM[9]={0,nt,tl,tp,ct,0,car,id,en};
+C* VT=" ~!@#+*:,";
 
-Q Ap(Q a){Q p=tsn(4,0,3,1);pid(p,0,a);return p;}
+Q Ap(Q a){Q p=tsn(4,1,3,1);pid(p,0,a);return p;}
 Q e(Q* q);
 Q emv(Q*q){
   Q w=e(q+1);
-  if(4==t(w)){Q p=tsn(4,0,3,1);pid(p,0,*q);return caVV(p,w);}
-  return VM[v(*q)][sh(w)](w);
+  if(4==t(w)){Q p=tsn(4,1,3,1);pid(p,0,*q);return ca(p,w);}
+  return VM[v(*q)](w);
 }
 Q edv(Q a,Q*q){
-  Q w=e(q+1);
-  if(4==t(w)&&(6!=v(*q))){Q p=tsn(4,0,3,2);pid(p,0,a);pid(p,1,*q);return caVV(p,w);}  // handle partial evaluations but allow assignment of them instantly. 
-  a=((1==t(a))&&(6!=v(*q)))?O[d(a)]:a;
-  DV dv=VD[v(*q)][sh(a)][sh(w)];
+  Q w=e(q+1);B i=v(*q); printf("verb idx %d\n",i);
+  if(4==t(w)&&(7!=i)){Q p=tsn(4,1,3,2);pid(p,0,a);pid(p,1,*q);return ca(p,w);}  // handle partial evaluations but allow assignment of them instantly. 
+  a=((1==t(a))&&(7!=i))?O[d(a)]:a;
+  DV dv=VD[i];
   return dv(a,w);
 }
 Q e(Q* q){
   Q a=*q;
-  if(!a){return tsn(4,0,3,0);}                                 // If a is the end of the stream then we must have missing data. return type 4
+  if(!a){return tsn(4,1,3,0);}                                 // If a is the end of the stream then we must have missing data. return type 4
   Q w=q[1];                                                    // we know a is non zero, so we can read q[1] but it may be end of stream, need to check if it is 0
   return (2==t(a)&&w)  ?                                       // if a is a verb and w isn't the end of the stream
          emv(q)        :                                       //  then evaluate a monadic verb
@@ -261,7 +264,7 @@ Q e(Q* q){
 
 Q R(C a){return ('a'<=a&&a<='z')?ar(a-'a'):0;} 
 Q N(C a){return ('0'<=a&&a<='9')?an(a-'0'):0;}
-Q V(C a){for(D i=0;i<8;i++){if(VT[i]==a){return av(i);}}return 0;} 
+Q V(C a){for(D i=0;i<9;i++){if(VT[i]==a){return av(i);}}return 0;} 
 Q* pe(C*b){D l=strlen(b);Q* q=malloc(sizeof(Q)*(l+1));D i=0;
   for(;i<l;i++){
     Q a;q[i]=(a=R(b[i]))?a:(a=V(b[i]))?a:(a=N(b[i]))?a:0;
